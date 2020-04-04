0a615fc609c5ee16d52fd77b1e4e244d86a93a3c
solo <solomonwachira212@gmail.com>
2020-04-04 20:52:01 +0300

    Java file that sends and retrieves data from the database

---
+++ app/src/main/java/com/awesome/stttt/Database.java
@@ -0,0 +1,58 @@
+package com.awesome.stttt;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+import androidx.annotation.Nullable;
+import com.android.volley.*;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Database {
+  private static final String TAG = "Database";
+   //
+    Context ctx;
+    public Database(Context context) {
+        ctx = context;
+        requestQueue = Volley.newRequestQueue(ctx);
+    }
+    Boolean check_credentials(JSONObject object){
+        senddata("user","check_client",object);
+        return true;
+    }
+    RequestQueue requestQueue;
+    public void senddata(final String classname, final String method, final JSONObject obj){
+        //
+        String url = "https://mutall.co.ke/volley/alter.php";
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "onResponse: Response " +response.toString());
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //
+                Log.d(TAG, "onErrorResponse: Error"+error.toString());
+            }
+
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("class", classname);
+                params.put("method", method);
+                params.put("data", String.valueOf(obj));
+                return params;
+            }
+        };
+        requestQueue.add(request);
+    }
+}
