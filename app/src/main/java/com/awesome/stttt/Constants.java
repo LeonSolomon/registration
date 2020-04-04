bb004ed9c4c068e96ef8910b9e51a68d659f2cd3
solo <solomonwachira212@gmail.com>
2020-04-04 20:52:38 +0300

    Java file that contains constants

---
+++ app/src/main/java/com/awesome/stttt/Constants.java
@@ -0,0 +1,8 @@
+package com.awesome.stttt;
+
+public class Constants {
+    private static final String url = "http://192.168.1.224/";
+    private static final String dbname = "volley";
+    private static final String username = "root";
+    private static final String password = "";
+}
